c77f518a9c04caa727d940fc0338376f5b4ebe40
steel <steel@10.215.197.235>
2018-03-20 23:20:11 -0400

    The lab for 3/20

---
+++ JOC113_SpotifyWeb/src/GetArtistList.java
@@ -0,0 +1,73 @@
+
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Servlet implementation class GetArtistList
+ */
+@WebServlet("/GetArtistList")
+public class GetArtistList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetArtistList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		try {
+			DbUtilities db = new DbUtilities();
+			String sql = "SELECT * FROM artist;";
+			ResultSet rs;
+			JSONArray artistList = new JSONArray();
+			rs = db.getResultSet(sql);
+			while (rs.next()) {
+				JSONObject artist = new JSONObject();
+				artist.put("id", rs.getString("artist_id"));
+				artist.put("id", rs.getString("first_name"));
+				artist.put("id", rs.getString("last_name"));
+				artist.put("id", rs.getString("band_name"));
+				artist.put("id", rs.getString("bio"));
+				
+				artistList.put(artist);
+			}
+			
+			response.getWriter().write(artistList.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
